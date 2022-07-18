068350c96d9c8a7d70c47492092599ad41206161
29488 <ppf@jiumao.org>
2022-07-18 19:52:01 +0800

    readme

--- network/poolformer.py
+++ network/poolformer.py
@@ -18,6 +18,9 @@ Cited from PoolFormer
 import torch
 import torch.nn as nn
 from timm.models.layers import DropPath
+from torch.nn.init import trunc_normal_
+
+from network.modules.blocks import ResnetBlock
 
 
 class GroupNorm(nn.GroupNorm):
@@ -44,6 +47,39 @@ class Pooling(nn.Module):
 
     def forward(self, x):
         return self.pool(x) - x
+
+
+class Mlp(nn.Module):
+    """
+    Implementation of MLP with 1*1 convolutions.
+    Input: tensor with shape [B, C, H, W]
+    """
+
+    def __init__(self, in_features, hidden_features=None, resblock=False,
+                 out_features=None, act_layer=nn.GELU, drop=0.):
+        super().__init__()
+        out_features = out_features or in_features
+        hidden_features = hidden_features or in_features
+        self.fc1 = ResnetBlock(in_features, hidden_features, 1)
+        self.act = act_layer()
+        self.fc2 = ResnetBlock(hidden_features, out_features, 1) if resblock else nn.Conv2d(hidden_features,
+                                                                                            out_features, 1)
+        self.drop = nn.Dropout(drop)
+        self.apply(self._init_weights)
+
+    def _init_weights(self, m):
+        if isinstance(m, nn.Conv2d):
+            trunc_normal_(m.weight, std=.02)
+            if m.bias is not None:
+                nn.init.constant_(m.bias, 0)
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.act(x)
+        x = self.drop(x)
+        x = self.fc2(x)
+        x = self.drop(x)
+        return x
 
 
 class PoolFormerBlock(nn.Module):
